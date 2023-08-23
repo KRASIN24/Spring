79cdfa9ecbb6f85d4e7d4468bf1ff4e4c8ec4152
Krasin <jakub.777@outlook.com>
2023-08-24 00:42:35 +0200

    Initial commit

---
+++ src/main/java/com/wishnest/WishNest/WishNestApplication.java
@@ -0,0 +1,13 @@
+package com.wishnest.WishNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishNestApplication.class, args);
+	}
+
+}
